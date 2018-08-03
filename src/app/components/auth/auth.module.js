4d666d9babcdf4201e577a5e6dd07826acc6cd88
Sergio Mazzoleni <sergio.mazzoleni@gmail.com>
2018-08-03 13:28:57 +0200

    firebase ok

--- src/app/components/auth/auth.module.js
+++ src/app/components/auth/auth.module.js
@@ -1,5 +1,17 @@
 angular
-  .module('components.auth', [
-    'ui.router',
-    'firebase'
-  ]);
+    .module('components.auth', ['ui.router', 'firebase'])
+    .config(function($firebaseRefProvider) {
+        var config = {
+            apiKey: 'AIzaSyDyGVDC9fH9GxRoQqMzDdYFxdDvI0MFpHo',
+            authDomain: 'contacts-manager-8ebb4.firebaseapp.com',
+            databaseURL: 'https://contacts-manager-8ebb4.firebaseio.com',
+            projectId: 'contacts-manager-8ebb4',
+            storageBucket: 'contacts-manager-8ebb4.appspot.com',
+            messagingSenderId: '389599506332',
+        };
+        $firebaseRefProvider.registerUrl({
+            default: config.databaseURL,
+            contacts: config.databaseURL + 'contacts',
+        });
+        firebase.initializeApp(config);
+    });
